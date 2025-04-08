b2ca4eab2dc3f339a7b24971578bc15157c07fbf
Vinay kuamr <masumansariedugaon@gmail.com>
2025-04-08 18:21:34 +0530

    Update signup_email_verification.dart
    i changed some codes SignupEmailVerification in  the app.

--- lib/views/screens/auth/signup/signup_email_verification.dart
+++ lib/views/screens/auth/signup/signup_email_verification.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 class SignupEmailVerification extends StatelessWidget {
   const SignupEmailVerification({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
