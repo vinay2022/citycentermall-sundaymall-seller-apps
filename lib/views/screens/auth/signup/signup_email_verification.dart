45853e9be23a76c339a895a64be4ed9885c7cbbf
Vinay kuamr <masumansariedugaon@gmail.com>
2025-04-08 18:07:48 +0530

    i created a signupemailverification screen in the app.

---
+++ lib/views/screens/auth/signup/signup_email_verification.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class SignupEmailVerification extends StatelessWidget {
+  const SignupEmailVerification({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Sign up Email Verification"),
+      ),
+    );
+  }
+}
